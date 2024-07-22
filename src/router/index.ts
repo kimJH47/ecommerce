2167fa3e4dd635385d1bed69ff1f8714184611bc
kimJaeHyeon <kmr2644@gmail.com>
2024-07-22 16:08:34 +0900

    chore: 라우터 컴포넌트 선언 변경

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignUpView from '@/views/SignupView.vue'
 import MainView from '@/views/MainView.vue'
+import EmailVerifyView from '@/views/EmailVerifyView.vue'
+import VerifiedCompleteView from '@/views/VerifiedCompleteView.vue'
+import CategoryView from '@/views/CategoryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,14 +25,19 @@ const router = createRouter({
     },
     { path: '/login', component: () => import('@/views/LoginView.vue') }, // 필요시 LoginView 생성
     //{ path: '/cart', component: () => import('@/views/CartView.vue') }, // 필요시 CartView 생성
-    { path: '/category/:category', component: () => import('@/views/CategoryView.vue') }, // 필요시 CategoryView 생성
+    { path: '/category/:category', component: CategoryView }, // 필요시 CategoryView 생성
 
-    { path: '/verify', component: () => import('@/views/EmailVerifyView.vue') },
+    {
+      path: '/verify',
+      name: 'emailVerifyView',
+      component: EmailVerifyView,
+      props: true
+    },
     {
       path: '/verified-complete',
-      name: 'VerifiedComplete',
-      component: () => import('@/views/VerifiedCompleteView.vue') }
-
+      name: 'VerifiedCompleteView',
+      component: VerifiedCompleteView
+    }
   ]
 })
 
