dd4498e2207ed0a4b98631a4bae210b848a268c2
kimJaeHyeon <kmr2644@gmail.com>
2024-07-24 19:31:37 +0900

    feat: searchWithCondition 구현

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,37 +1,40 @@
 import { convertToSearchParams } from '@/utils/Converter'
 import type { Product } from '@/services/interfaces/Product'
+import type { ProductSearchCondition } from '@/services/interfaces/ProductSearchCondition'
 
 export async function searchFirstPage(category: string): Promise<Product[]> {
-  return fetch(`http://localhost:8080/api/v2/categoires/${category}`)
+  return fetch(`http://localhost:8080/api/v2/categories/${category}`)
     .then(res => {
       if (!res.ok) {
         throw new Error(res.statusText)
       }
       return res.json()
     }).then(data => {
+      console.log(data)
       return data.entity.products
     }).catch(err => {
       console.log(err)
     })
 }
 
-export function searchProducts(
-  category: string, page: number, SortType: SortType, name: string, brandName: string
-): Promise<Product[]> {
+export async function searchWithCondition(condition: ProductSearchCondition): Promise<Product[]> {
   const params = convertToSearchParams({
-    page: page,
-    name: name,
-    brandName: brandName,
-    category: category,
-    sort: SortType
+    page: condition.page,
+    name: condition.searchQuery,
+    brandName: condition.searchQuery,
+    category: condition.category,
+    sort: condition.sortOrder,
+    maxPrice: condition.maxPrice,
+    minPrice: condition.minPrice
   })
-  return fetch(`http://localhost:8080/api/v2/categoires/${category}/detail?${params}`)
+  return fetch(`http://localhost:8080/api/v2/categories/${condition.category}/detail?${params}`)
     .then(res => {
       if (!res.ok) {
         throw new Error(res.statusText)
       }
       return res.json()
     }).then(data => {
+      console.log(data)
       return data.entity.products
     }).catch(err => {
       console.log(err)
