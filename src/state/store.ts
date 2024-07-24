0dbef57030a54070668874ecea632dba7eb2347d
kimJaeHyeon <kmr2644@gmail.com>
2024-07-24 19:30:38 +0900

    feat: 검색 컨디션 스토어 구현

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia'
+import type { ProductSearchCondition } from '@/services/interfaces/ProductSearchCondition'
 
 export const useStatusStore = defineStore('status', {
   state: () => ({
@@ -8,5 +9,36 @@ export const useStatusStore = defineStore('status', {
     setSuccess(status: boolean) {
       this.isSuccess = status
     }
+  }
+})
+
+export const useSearchConditionStore = defineStore('condition', {
+  state: () => ({
+    page: 1,
+    category: '',
+    searchQuery: null as string | null,
+    searchType: 'name',
+    sortOrder: 'new',
+    minPrice: null as number | null,
+    maxPrice: null as number | null
+  }),
+  actions: {
+    updateCondition(condition: ProductSearchCondition) {
+      this.searchQuery = condition.searchQuery
+      this.sortOrder = condition.sortOrder
+      this.minPrice = condition.minPrice
+      this.maxPrice = condition.maxPrice
+      //this.searchType=condition.searchType
+    }
+  },
+  getters: {
+    condition: (state) => ({
+      page: 1,
+      category: state.category,
+      sortOrder: state.sortOrder,
+      searchQuery: state.searchQuery,
+      minPrice: state.minPrice,
+      maxPrice: state.maxPrice
+    })
   }
 })
